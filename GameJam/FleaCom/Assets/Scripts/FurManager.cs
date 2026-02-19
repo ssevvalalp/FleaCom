0b10da735a2bd2614645c932c5e66cf6d6084f2b
Şevval Alp <ssevvalalp@gmail.com>
2026-02-20 00:39:00 +0300

    FurManager fixed

--- GameJam/FleaCom/Assets/Scripts/FurManager.cs
+++ GameJam/FleaCom/Assets/Scripts/FurManager.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Net.NetworkInformation;
+﻿using System;
 using UnityEngine;
-
 
 public class FurManager : MonoBehaviour
 {
-
     [SerializeField] private GameObject furPrefab;
     [SerializeField] private Vector2 origin;
     [SerializeField] private int borderX, borderY;
@@ -18,13 +15,21 @@ public class FurManager : MonoBehaviour
 
     private void PlaceFurs()
     {
-        int x = 0, y = 0;
+        int x, y;
         Vector3 spawnPosition;
         int i = 0;
 
-        while (i < furCount) 
+        // Sonsuz döngü kilidini önlemek için güvenlik sayacı
+        int attempts = 0;
+        int maxAttempts = furCount * 10;
+        
+
+        while (i < furCount && attempts < maxAttempts)
         {
-            GenerateXY(x, y);
+            attempts++; // Her denemede sayacı artır
+
+            // 'out' anahtar kelimesi ile GenerateXY içindeki değerleri buraya çekiyoruz
+            GenerateXY(out x, out y);
             spawnPosition = new Vector3(origin.x + x, origin.y + y);
 
             if (CanSpawnFur(spawnPosition))
@@ -33,28 +38,33 @@ public class FurManager : MonoBehaviour
                 i++;
             }
         }
-   
+
+        Debug.Log("toplam yerleşen: " + i.ToString());
+
+        // Eğer alanda yer kalmadıysa bizi uyarması için:
+        if (attempts >= maxAttempts)
+        {
+            Debug.LogWarning($"Alan çok dar! İstenen {furCount} pireden sadece {i} tanesi yerleştirilebildi.");
+        }
     }
 
-    private void GenerateXY(int x, int y)
+    // Metodun parametrelerine 'out' ekledik
+    private void GenerateXY(out int x, out int y)
     {
         x = UnityEngine.Random.Range(-borderX, borderX + 1);
-        y = UnityEngine.Random.Range(-borderY, borderY + 1);      
+        y = UnityEngine.Random.Range(-borderY, borderY + 1);
     }
 
     public bool CanSpawnFur(Vector3 position)
     {
         BoxCollider2D boxCollider2D = furPrefab.GetComponent<BoxCollider2D>();
 
+        // OverlapBox için çakışma kontrolü
         if (Physics2D.OverlapBox(position + (Vector3)boxCollider2D.offset, boxCollider2D.size, 0) != null)
         {
             return false;
         }
-        else
-        {
-            return true;
-        }
+
+        return true;
     }
-
 }
-
